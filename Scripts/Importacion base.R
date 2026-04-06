b91da6ea90b25dc8a772d6b521f5dcaecb1d056e
mercedespaz <pazmercedes.mp@gmail.com>
2026-04-05 21:11:14 -0300

    Scripts para procesamiento de datos

---
+++ Scripts/Importacion base.R
@@ -0,0 +1,3 @@
+# IMPORTACIÓN BASE NOMINAL UC-IRAG
+
+base <-read.csv2("Data/UC_IRAG_NOMINAL.csv", sep= ";", encoding = "latin1")
---
+++ Scripts/Librerias.R
@@ -0,0 +1,9 @@
+# ACTIVACIÓN DE LIBRERÍAS A UTILIZAR 
+
+library(ggplot2)
+library(tidyr)
+library(dplyr)
+library(readr)
+library(lubridate)
+library(stringr)
+library(highcharter)
